56f126251a6c572c829ffed7461696c358074035
Victor Yamchinov <kodiers@gmail.com>
2019-11-27 00:23:46 +0300

    Initial Commit

---
+++ xyz-machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Viktor Yamchinov on 27.11.2019.
+//  Copyright © 2019 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
